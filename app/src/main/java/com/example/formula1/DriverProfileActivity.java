2f486276bb20cb5e39fd1372b0ab06a6b062ce69
andreeagorcsa <andreea.gorcsa@t-systems.com>
2020-08-25 22:35:14 +0200

    Add DriverProfileActivity which will contain the profile of each selected driver.

---
+++ app/src/main/java/com/example/formula1/DriverProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.formula1;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DriverProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_profile);
+    }
+}
