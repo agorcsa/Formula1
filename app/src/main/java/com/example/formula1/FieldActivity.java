9f2f91ff51ea55230844afbd88eba64ab5c52217
andreeagorcsa <andreea.gorcsa@t-systems.com>
2020-08-25 22:35:41 +0200

    Add SearchView

--- app/src/main/java/com/example/formula1/FieldActivity.java
+++ app/src/main/java/com/example/formula1/FieldActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,6 +23,8 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 
@@ -27,7 +34,7 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class FieldActivity extends AppCompatActivity implements DriverAdapter.DriverInfoButtonClick{
+public class FieldActivity extends AppCompatActivity implements DriverAdapter.OnClick{
 
     private RecyclerView mDriverRecyclerView;
     private RecyclerView.Adapter mDriverAdapter;
@@ -71,16 +78,17 @@ public class FieldActivity extends AppCompatActivity implements DriverAdapter.Dr
                 DriverTable driverTable = mrData.getDriverTable();
 
                 List<Driver> list = driverTable.getDriverList();
-
-                int natCounter = 0;
-                int index = 0;
-
+                ArrayList<String> nationalities = new ArrayList<>();
                 for (Driver driver : list) {
-                    if(driver.getNationality().equals(list.get(index).getNationality())) {
-                        natCounter++;
-                        String nat = list.get(index).getNationality();
-                        counter.setText(nat +": " + String.valueOf(natCounter));
-                    }
+                    nationalities.add(driver.getNationality());
+                }
+                List<String> uniqueNationalities = new ArrayList<>(new HashSet<>(nationalities));
+                for (String nationality: uniqueNationalities) {
+                    int count = Collections.frequency(nationalities, nationality);
+                    counter.append(nationality +": " + count + "\n");
+
+                    //Comparator c = Collections.reverseOrder();
+                    //Collections.sort(list,c);
                 }
 
                 mDriverArrayList.clear();
@@ -110,5 +118,36 @@ public class FieldActivity extends AppCompatActivity implements DriverAdapter.Dr
     public void onDriverInfoButtonClick(Uri uri, View view) {
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
         startActivity(intent);
+    }
+
+    @Override
+    public void onItemClick(View v) {
+        Intent intent = new Intent(FieldActivity.this, DriverProfileActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        final MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Toast.makeText(getApplicationContext(), "SearchView", Toast.LENGTH_LONG).show();
+
+                //mDriverAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
     }
 }
