649d671a822d5de3028fb42d07bf33e5d092bbd8
andreeagorcsa <andreea.gorcsa@t-systems.com>
2020-08-26 09:34:25 +0200

    Add JsonCompetitorApi which will call the api for driverId.

---
+++ app/src/main/java/com/example/formula1/api/JsonCompetitorApi.java
@@ -0,0 +1,15 @@
+package com.example.formula1.api;
+
+import com.example.formula1.object.Competitor;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonCompetitorApi {
+    // full url example
+    // http://ergast.com/api/f1/drivers/ricciardo.json  
+    // relative url
+    // driverId, example: ricciardo.json
+    @GET("ricciardo.json")
+    Call<ApiResponse> getCompetitor();
+}
